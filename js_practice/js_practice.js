b4e31dfa588e0ec867b91589dcee1a9c8b559b30
OlyaPinchuk <olha5959@gmail.com>
2020-10-29 01:27:19 +0200

    Add js files.

---
+++ js_practice/js_practice.js
@@ -0,0 +1,142 @@
+// // let first = 'hello';
+
+// // alert(first);
+// // console.log(first);
+// // document.write(first);
+
+// // first = 'I and you';
+// // alert(first);
+
+// // let last_name = prompt('Enter your last name:')
+// // // document.write(last_name);
+
+// // let name = prompt('Enter your name:')
+// // // document.write(name);
+
+// // let third_name = prompt('Enter your по-батькові:')
+// // // document.write(third_name);
+
+// // let person = last_name + name + third_name;
+
+// // document.write(person);
+
+// let number1 = parseFloat(prompt('>>'));
+// let number2 = +prompt('>>');
+// let number3 = parseInt(prompt('>>'));
+
+// let result = number1 + number2 + number3;
+// console.log(result);
+
+
+// class2
+
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 *3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+
+// class3
+
+// let a6 = 5 % 3;
+// let a7 = 3 % 5;
+// let a8 = 5 + '3';
+// let a9 = '5' - 3;
+// let a10 = 75 + 'kg';
+
+// class4
+
+// let height = '23cm';
+// let width = '10cm';
+// let s = parseInt(height) * parseInt(width);
+// console.log(s)
+
+// class5
+
+// let heightC = '10m'
+// let dC = '4m';
+// let v = parseInt(heightC) * parseInt(dC);
+// console.log(v);
+
+// class6
+
+// let n = 3;
+// let m = 4;
+// let k = n**2 + m**2;
+// console.log(k)
+
+// class7
+
+// let str = 'Hello world!'
+// alert(str);
+// console.log(str);
+// document.write(str);
+
+// class8
+
+// alert('Olya \nPinchuk \n22 \ndancing')
+
+// class9
+
+// let str1 = 'Who ';
+// let str2 = 'are ';
+// let str3 = 'you';
+// let str4 = '?';
+// let concatenation = str1 + str2 +str3 +str4;
+
+// document.write(concatenation)
+
+// class10
+
+// let str = '20';
+// let a = 5;
+// document.write(str + a + '<br/>');
+// document.write(str - a + '<br/>');
+// document.write(str * '2' + '<br/>');
+// document.write(str / 2 + '<br/>');
+
+
+// class11
+
+// console.log(
+// parseInt('3.14'), 
+// parseInt('-7.875'), 
+// parseInt("435"), 
+// parseInt('Vasyl'))
+
+// class12
+
+// class13
+// let n1 = +prompt('Enter a number:');
+// let n2 = +prompt('Enter a number:');
+// let result = n1 + n2;
+// alert(result);
+
+// class14
+
+// let name = prompt('Enter your name:');
+// let last_name = prompt ('Enter your last name:');
+// let age = prompt('Enter your age:');
+
+// alert('Good evening ' + name + ' ' + last_name + ' my congrats you are' + age + '!')
+
+// additional1
+
+// let n1 = +prompt('>>')
+// let n2 = +prompt('>>')
+// let number3 = +prompt('>>')
+
+// if (n1 )
+
+
+
+
+
+
+
+
+
+
+
+
+
