cebffad05241e7fb5256731dc70ceb42df7ee407
OlyaPinchuk <olha5959@gmail.com>
2020-10-29 03:09:38 +0200

    Add js hometask.

--- js_practice/js_practice.js
+++ js_practice/js_practice.js
@@ -1,34 +1,282 @@
-// // let first = 'hello';
 
-// // alert(first);
-// // console.log(first);
-// // document.write(first);
+// HOMEWORK 1
 
-// // first = 'I and you';
-// // alert(first);
+// let v1 = 'owu';
+// let v2 = 'com';
+// let v3 = 'ua';
+// let v4 = 1;
+// let v5 = 10;
+// let v6 = -999;
+// let v7 = 123;
+// let v8 = 3.14;
+// let v9 = 2.7;
+// let v10 = 16;
+// let v11 = true;
+// let v12 = false;
 
-// // let last_name = prompt('Enter your last name:')
-// // // document.write(last_name);
+// HOMEWORK 2
 
-// // let name = prompt('Enter your name:')
-// // // document.write(name);
+// v1 = 'Hi';
+// v2 = 'you';
+// v3 = 'here';
+// v4 = 6;
+// v5 = 5;
+// v6 = -12;
+// v7 = 678;
+// v8 = 4.5;
+// v9 = 8.7;
+// v10 = 189;
+// v11 = false;
+// v12 = true;
 
-// // let third_name = prompt('Enter your по-батькові:')
-// // // document.write(third_name);
+// alert(v1)
+// console.log(v1)
+// document.write(v1)
 
-// // let person = last_name + name + third_name;
+// alert(v2)
+// console.log(v2)
+// document.write(v2)
 
-// // document.write(person);
+// alert(v3)
+// console.log(v3)
+// document.write(v3)
 
-// let number1 = parseFloat(prompt('>>'));
-// let number2 = +prompt('>>');
-// let number3 = parseInt(prompt('>>'));
+// alert(v4)
+// console.log(v4)
+// document.write(v4)
 
-// let result = number1 + number2 + number3;
+// alert(v5)
+// console.log(v5)
+// document.write(v5)
+
+// alert(v6)
+// console.log(v6)
+// document.write(v6)
+
+// alert(v7)
+// console.log(v7)
+// document.write(v7)
+
+// alert(v8)
+// console.log(v8)
+// document.write(v8)
+
+// alert(v9)
+// console.log(v9)
+// document.write(v9)
+
+// alert(v10)
+// console.log(v10)
+// document.write(v10)
+
+// alert(v11)
+// console.log(v11)
+// document.write(v11)
+
+// alert(v12)
+// console.log(v12)
+// document.write(v12)
+
+// HOMEWORK 3
+
+// const num1 = 12;
+// const num2 = 23;
+// const num3 = 34;
+
+// const str1 = 'I';
+// const str2 = 'am';
+// const str3 = 'here';
+
+// alert(num1);
+// console.log(num1);
+// document.write(num1);
+
+// alert(num2);
+// console.log(num2);
+// document.write(num2);
+
+// alert(num3);
+// console.log(num3);
+// document.write(num3);
+
+// alert(str1);
+// console.log(str1);
+// document.write(str1);
+
+// alert(str2);
+// console.log(str2);
+// document.write(str2);
+
+// alert(str3);
+// console.log(str3);
+// document.write(str3);
+
+// HOMEWORK 4
+
+// let last_name = prompt('Прізвище:');
+// let name = prompt("Ім'я:");
+// let father_name = prompt('По-батькові:');
+
+// alert(last_name);
+// console.log(last_name);
+// document.write(last_name);
+
+// alert(name);
+// console.log(name);
+// document.write(name);
+
+// alert(father_name);
+// console.log(father_name);
+// document.write(father_name);
+
+
+// HOMEWORK 5
+
+// let last_name = prompt('Прізвище:');
+// let name = prompt("Ім'я:");
+// let father_name = prompt('По-батькові:');
+
+// let person = last_name + ' ' + name + ' ' + father_name;
+// console.log(person);
+
+// HOMEWORK 6
+
+// HOMEWORK 7
+
+// let num1 = +prompt('Enter a number:');
+// let num2 = +prompt('Enter a number:');
+// let num3 = +prompt('Enter a number:');
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+
+// HOMEWORK 8
+
+// let num1 = parseInt(prompt('Enter a number:'));
+// let num2 = parseInt(prompt('Enter a number:'));
+// let num3 = parseInt(prompt('Enter a number:'));
+// let num4 = parseInt(prompt('Enter a number:'));
+
+// let result = num1 + num2 + num3 + num4;
 // console.log(result);
 
 
-// class2
+// HOMEWORK 9
+
+// let num1 = parseFloat(prompt('Enter a number:'));
+// let num2 = parseFloat(prompt('Enter a number:'));
+// let num3 = parseFloat(prompt('Enter a number:'));
+
+// let result = num1 + num2 + num3;
+// console.log(result);
+
+
+// HOMEWORK 10
+
+// let num1 = Math.round(prompt('Enter a number:'));
+// let num2 = Math.round(prompt('Enter a number:'));
+// let num3 = Math.round(prompt('Enter a number:'));
+
+// let result = num1 + num2 + num3;
+// console.log(result);
+
+// HOMEWORK 11
+
+// let num1 = parseInt(prompt('Enter a number:'));
+// let num2 = parseInt(prompt('Enter a number:'));
+
+
+// let result = Math.pow(num1, num2);
+// console.log(result);
+
+// HOMEWORK 12
+
+// let a = 100; 
+// let b = '100'; 
+// let c = true; 
+// let d = undefined;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// HOMEWORK 13,14,15
+
+// let a = 5 < 6 
+// // -> true
+// let b = 5 > 6 
+// // -> false
+// let c = 5 == 6 
+// // -> false
+// let d = 5 >= 6 
+// // -> false
+// let e = 10 == 10 
+// // -> true
+// let f = 10 <= 10 
+// // -> true
+// let g = 10 > 10 
+// // -> false
+// let h = 10 < 10 
+// // -> false
+// let i = 10 != 10 
+// // -> false
+// let j = 123 === '123' 
+// // -> false
+// let k = 123 == '123' 
+// // -> true
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CLASS1
+
+// let str = 'Hi'
+// let n = 45
+// console.log(typeof n)
+
+
+// CLASS2
 
 // let a1 = 5 + 3;
 // let a2 = 5 - 3;
@@ -36,7 +284,7 @@
 // let a4 = 5 / 3;
 // let a5 = 5 % 3;
 
-// class3
+// CLASS3
 
 // let a6 = 5 % 3;
 // let a7 = 3 % 5;
@@ -44,39 +292,39 @@
 // let a9 = '5' - 3;
 // let a10 = 75 + 'kg';
 
-// class4
+// CLASS4
 
 // let height = '23cm';
 // let width = '10cm';
 // let s = parseInt(height) * parseInt(width);
 // console.log(s)
 
-// class5
+// CLASS5
 
 // let heightC = '10m'
 // let dC = '4m';
 // let v = parseInt(heightC) * parseInt(dC);
 // console.log(v);
 
-// class6
+// CLASS6
 
 // let n = 3;
 // let m = 4;
 // let k = n**2 + m**2;
 // console.log(k)
 
-// class7
+// CLASS7
 
 // let str = 'Hello world!'
 // alert(str);
 // console.log(str);
 // document.write(str);
 
-// class8
+// CLASS8
 
 // alert('Olya \nPinchuk \n22 \ndancing')
 
-// class9
+// CLASS9
 
 // let str1 = 'Who ';
 // let str2 = 'are ';
@@ -86,7 +334,7 @@
 
 // document.write(concatenation)
 
-// class10
+// CLASS10
 
 // let str = '20';
 // let a = 5;
@@ -95,8 +343,7 @@
 // document.write(str * '2' + '<br/>');
 // document.write(str / 2 + '<br/>');
 
-
-// class11
+// CLASS11
 
 // console.log(
 // parseInt('3.14'), 
@@ -104,7 +351,7 @@
 // parseInt("435"), 
 // parseInt('Vasyl'))
 
-// class12
+// CLASS12
 
 // class13
 // let n1 = +prompt('Enter a number:');
@@ -112,21 +359,41 @@
 // let result = n1 + n2;
 // alert(result);
 
-// class14
+// CLASS14
 
 // let name = prompt('Enter your name:');
 // let last_name = prompt ('Enter your last name:');
 // let age = prompt('Enter your age:');
+// alert('Good evening ' + name + ' ' + last_name + ' my congrats you are ' + age + '!')
 
-// alert('Good evening ' + name + ' ' + last_name + ' my congrats you are' + age + '!')
-
-// additional1
+// ADDITIONAL 1
 
 // let n1 = +prompt('>>')
 // let n2 = +prompt('>>')
-// let number3 = +prompt('>>')
+// let n3 = +prompt('>>')
 
-// if (n1 )
+// if (n1 < n2 && n2 < n3) {
+//     console.log(n1, n2, n3)
+
+// } else if (n1 < n3 && n3 < n2) {
+//     console.log(n1, n3, n2)
+
+// } else if (n2 < n1 && n1 < n3) {
+//     console.log(n2, n1, n3)
+
+// } else if (n2 < n3 && n3 < n1) {
+//     console.log(n2, n3, n1)
+
+// } else if (n3 < n1 && n1 < n2) {
+//     console.log(n3, n1, n2)
+
+// } else if (n3 < n2 && n2 < n1) {
+//     console.log(n3, n2, n1)
+
+// } else {
+//     console.log('Error')
+
+// }
 
 
 
