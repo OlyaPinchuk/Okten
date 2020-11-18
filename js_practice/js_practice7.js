f013b49f545efa28c33b5839eda6c5ade36d5c94
OlyaPinchuk <olha5959@gmail.com>
2020-11-18 19:12:31 +0200

    Change js 7.

--- js_practice/js_practice7.js
+++ js_practice/js_practice7.js
@@ -284,32 +284,6 @@
     
 // ======================================================================================================================================================= 
         
-// let input = document.getElementById('thisInput')
-// let submitButton = document.getElementById('submit')
-// let form = document.getElementById('myForm')
-
-// document.myForm.submit.addEventListener('click', (ev) => {
-    
-//     ev.preventDefault()
-// })
-
-
-// =================================================/
-
-// let textArea = document.getElementById('textarea');
-
-// textArea.onchange = () => {
-    
-//     localStorage.setItem('userText', `${textArea.value}`)
-// }
-
-
-// document.addEventListener('DOMContentLoaded', (ev) => {
-
-//     let user = localStorage.getItem('userText')
-//     textArea.value = user
-//     console.log(user)
-// })
 
 
         
